09734ebc36b35fe014a9515bb01d7a9a513c9314
ext_daothanh <ext_daothanh@asseco-ce.com>
2025-10-13 13:22:09 +0700

    add method

---
+++ method/1.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func func4() {
+	fmt.Println("Func4: ")
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
+}
+
+func Scale(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func func5() {
+	fmt.Println("func 5: ")
+	v := Vertex{3, 4}
+	Scale(&v, 10)
+	fmt.Println(Abs(v))
+}
+
+func func1() {
+	fmt.Println("Func1: ")
+	v := Vertex{3,4}
+	fmt.Println(v.Abs())
+}
+
+func func2() {
+	fmt.Println("Func2: ")
+	v := Vertex{3,4}
+	fmt.Println(Abs(v))
+}
+
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func func6()  {
+	fmt.Println("func 6: ")
+	v := Vertex{3,4}
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+
+	p:= Vertex{4,3}
+	p.Scale(3)
+	ScaleFunc(&p, 8)
+
+	fmt.Println(v, p)
+
+}
+
+
+
