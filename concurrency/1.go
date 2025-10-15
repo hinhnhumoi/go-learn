056b2b0ba9e13eb926f5b4022c4e85179f781e0d
ext_daothanh <ext_daothanh@asseco-ce.com>
2025-10-15 14:20:57 +0700

    concurrency

---
+++ concurrency/1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func func1() {
+	fmt.Println("func 1: ")
+	go say("world")
+	say("hello")
+}
---
+++ concurrency/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	func1()
+	func2()
+	func3()
+	func4()
+	func5()
+	func6()
+	func7()
+	func9()
+	func10()
+}
